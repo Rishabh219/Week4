e8ed3b73ed095a1344037e190d341f9fa78a0e60
Rishabh219 <tiwaririshabh707@gmail.com>
2024-09-01 00:59:15 +1000

    Added functionality to deactivate Red GameObject and disable Blue Renderer

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,25 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine;
+
+using UnityEngine;
+
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;  // Renderer reference
 
-    private int i = 4;
+    private int i;
+    private int randomValue;
 
     void Start()
     {
-        
+        i = 5;  // Initialize i to 5
+        randomValue = Random.Range(150, 251);  // Generate a random integer between 150 and 250 inclusive
     }
 
     void Update()
     {
-        
-        Debug.Log(gameObject.name + ":" + i);
-        
+        Debug.Log(gameObject.name + "(Clone):" + i);
         i++;
+
+        if (tag == "Red" && i >= 100)
+        {
+            // Deactivate the game object if tag is "Red" and i is 100 or more
+            gameObject.SetActive(false);
+        }
+        else if (tag == "Blue" && i >= randomValue)
+        {
+            // Disable the Renderer component if tag is "Blue" and i is greater than or equal to the random value
+            if (rend != null)
+            {
+                rend.enabled = false;
+            }
+        }
     }
 }
+
+
 
 
 
