a63d2a8e477bf356c42d78a404bdf79dfaea1bc4
Rishabh219 <tiwaririshabh707@gmail.com>
2024-09-01 00:14:29 +1000

    Added ConsolePrint script to print GameObject name and incrementing integer

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    private int i = 3;
+
+    void Start()
+    {
+        
+        i++;
+    }
+
+    void Update()
+    {
+        i++;
+        
+        Debug.Log(gameObject.name + ":" + i);
+    }
+}
+
